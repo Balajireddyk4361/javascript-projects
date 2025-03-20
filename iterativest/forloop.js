75b84fba1c40356df69eb7bae83f2589f3e7b46d
Balajireddyk4361 <balajireddyk4451@gmail.com>
2025-03-20 14:48:40 +0530

    javascript

---
+++ iterativest/forloop.js
@@ -0,0 +1,15 @@
+// for(initialisation statement;conditional st; increment/decrement st)
+
+//WRA program to print 1 to 100 using for loop
+// for(let i=1;i<=100;i=i+1){
+//     console.log(i)
+// }
+
+
+//WRA program to print 100 to 51 using for loop
+// for(i=100;i>=51;i=i-1)
+//     console.log(i)
+
+//WRA program to print 100 to 51 using for loop
+for(i=1;i<=10 %2 == 0 ;i=i+1)
+    console.log(i)
