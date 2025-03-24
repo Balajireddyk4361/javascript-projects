ac3812f72f7f41aafd53fac433620a16e4997ff4
Balajireddyk4361 <balajireddyk4451@gmail.com>
2025-03-24 12:50:40 +0530

    javascript

---
+++ ArrowFunctions/return.js
@@ -0,0 +1,36 @@
+
+
+// function add(a,b,c){
+//     console.log(a+b+c)
+// }
+// add(10,20,30)
+
+// function to fat Arrow function
+// alter native syntax to writing aprogram is called arrow function
+
+
+
+//simple arrow function 
+
+// let add =(a,b,c)=>{
+//     console.log(a+b+c)
+// }
+// add(10,40,50)
+
+
+
+// implicit return
+// if your function as only one statement ie return ; we can avoid return
+
+
+//add using return 
+let z=(a,b)=>{
+    return a+b+1
+}
+let r1=z(10,20)
+console.log
+
+
+//setInterval(()=>{console.log('GM')},3000)
+setTimeout(()=>{console.log('GM')},3000)
+
--- functions/one.js
+++
@@ -1,7 +0,0 @@
-function get_Account_Bal(){
-    return 1000000;
-    console.log('gm')
-
-}
-let bal= get_Account_Bal()
-console.log(bal);
